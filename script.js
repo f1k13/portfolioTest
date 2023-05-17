f647fe6d71b78b947ad55f4866eb1bd0797d88d4
f1k13 <124521323+f1k13@users.noreply.github.com>
2023-05-17 15:56:18 +0300

    add

---
+++ script.js
@@ -0,0 +1,6 @@
+function modalOpen(){
+    document.querySelector('.modal').classList.add('modal__active')
+}
+function modalClose(){
+    document.querySelector('.modal').classList.remove('modal__active')
+}
