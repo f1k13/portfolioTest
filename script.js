1add6a36ea98a82df383d6cd9de16c4e32cf5a7e
f1k13 <124521323+f1k13@users.noreply.github.com>
2023-05-18 15:02:49 +0300

    new

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function modalOpen(){
+const btnOpen = document.querySelector('.btn__contact')
+const btnClose = document.querySelector('.close-modal')
+const burgerMenu = document.querySelector('.navbar__burger');
+const navMenu = document.querySelector('.navbar__list');
+
+btnOpen.addEventListener('click', () => {
     document.querySelector('.modal').classList.add('modal__active')
-}
-function modalClose(){
+})
+
+btnClose.addEventListener('click',() => {
     document.querySelector('.modal').classList.remove('modal__active')
-}
+})
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+document.querySelectorAll('.navbar__list').forEach(n => n.addEventListener('click', () => {
+    burgerMenu.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
+
